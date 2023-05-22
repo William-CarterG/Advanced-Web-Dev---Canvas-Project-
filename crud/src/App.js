617e2a51973c0fb94a2d1fce6fbe303735057a1c
William-CarterG <wgcarter@miuandes.cl>
2023-05-22 05:33:13 -0400

    style(crud-menu): Pressed/Unpressed buttons
    Implemented a class for buttons, which aims to improve the user interaction with them. It does it by better showing which button is currently pressed at every given moment.

--- crud/src/App.js
+++ crud/src/App.js
@@ -22,7 +22,43 @@ function handleTestsButtonClick() {
   console.log("Tests CRUD clicked.");
 }
 
+function buttonPressed(button) {
+  button.classList.add('pressed-button');
+}
+
+function unpressedButtons() {
+  const buttons = document.querySelectorAll('.button');
+  buttons.forEach((button) => {
+    button.classList.remove('pressed-button');
+  });
+}
+
 function App() {
+  useEffect(() => {
+    // No need to attach event listeners manually
+    // Instead, handle button clicks within the component
+
+    // Function to handle button click and add the 'pressed' class
+    function handleClick(event) {
+      unpressedButtons(); // Remove 'pressed' class from all buttons
+      const button = event.target;
+      buttonPressed(button);
+    }
+
+    // Attach click event listeners to the buttons
+    const buttons = document.querySelectorAll('.button');
+    buttons.forEach((button) => {
+      button.addEventListener('click', handleClick);
+    });
+
+    // Clean up the event listeners when the component unmounts
+    return () => {
+      buttons.forEach((button) => {
+        button.removeEventListener('click', handleClick);
+      });
+    };
+  }, []);
+
   return (
     <>
       <h1>Welcome to the Administration Panel</h1>
