ca099692f9b89bba6df4f73095072497bbc1b707
William-CarterG <wgcarter@miuandes.cl>
2023-06-09 18:58:07 -0400

    fix(group-table): color selection UI
    Fixed an error where the color of the brush would never change.

--- crud/src/app/components/groups/addGroup.js
+++ crud/src/app/components/groups/addGroup.js
@@ -143,68 +143,68 @@ function AddGroup() {
           </div>
         )}
         {currentPage === 2 && (
-      <div className="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
-        <div
-          onClick={toggleModelOpen}
-          className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40"
-          aria-hidden="true"
-        />
+          <div className="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
+            <div
+              onClick={toggleModelOpen}
+              className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-40"
+              aria-hidden="true"
+            />
 
-        <div className="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
-          <div className="flex items-center justify-between space-x-4">
-            <h1 className="text-xl font-medium text-gray-800">
-              Select a visual style for the group.
-            </h1>
-            <div className=''>
-              <button
-                onClick={goToPreviousPage}
-                className="mr-3 text-gray-600 focus:outline-none hover:text-gray-700"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path d="M10 16l-6-6 6-6"/>
-                  <path d="M20 21v-7a4 4 0 0 0-4-4H5"/>
-                </svg>
-              </button>
+            <div className="inline-block w-full max-w-xl p-8 my-20 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl 2xl:max-w-2xl">
+              <div className="flex items-center justify-between space-x-4">
+                <h1 className="text-xl font-medium text-gray-800">
+                  Select a visual style for the group.
+                </h1>
+                <div className=''>
+                  <button
+                    onClick={goToPreviousPage}
+                    className="mr-3 text-gray-600 focus:outline-none hover:text-gray-700"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                      <path d="M10 16l-6-6 6-6"/>
+                      <path d="M20 21v-7a4 4 0 0 0-4-4H5"/>
+                    </svg>
+                  </button>
 
-              <button
-                onClick={toggleModelOpen}
-                className="text-gray-600 focus:outline-none hover:text-gray-700"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-6 h-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-                  />
-                </svg>
-              </button>
+                  <button
+                    onClick={toggleModelOpen}
+                    className="text-gray-600 focus:outline-none hover:text-gray-700"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <p className="mt-2 text-sm text-gray-500">
+                  Select your settings to determine how you want your group to look.
+              </p>
+              <form onSubmit={handleVisualStyleSubmit} className="mt-5">
+                {/*<UploadFilesComponent />*/}
+                <ColorSelectionComponent name={"Background"}/>
+                <ColorSelectionComponent name={"Text Font"}/>
+                <div className="flex justify-end mt-6">
+                  <button onClick={goToNextPage}
+                  className="pressed-button flex items-center justify-center px-3 py-2 space-x-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-300 rounded-md focus:outline-none focus:ring focus:ring-opacity-50">
+                    Next Page
+                    <svg className="ml-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"/><path d="M12 8l4 4-4 4M8 12h7"/></svg>
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
-          <p className="mt-2 text-sm text-gray-500">
-              Select your settings to determine how you want your group to look.
-          </p>
-          <form onSubmit={handleVisualStyleSubmit} className="mt-5">
-            {/*<UploadFilesComponent />*/}
-            <ColorSelectionComponent name={"Background"}/>
-            <ColorSelectionComponent name={"Text Font"}/>
-            <div className="flex justify-end mt-6">
-              <button onClick={goToNextPage}
-              className="pressed-button flex items-center justify-center px-3 py-2 space-x-2 text-sm tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-300 rounded-md focus:outline-none focus:ring focus:ring-opacity-50">
-                Next Page
-                <svg className="ml-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"/><path d="M12 8l4 4-4 4M8 12h7"/></svg>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-)}
+        )}
 
         {currentPage === 3 && (
           <div className="flex items-end justify-center min-h-screen px-4 text-center md:items-center sm:block sm:p-0">
