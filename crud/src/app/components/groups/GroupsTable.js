fb6c39fd788176dea4d0ebaf163fa2e644c070a8
William-CarterG <wgcarter@miuandes.cl>
2023-06-09 23:48:55 -0400

    fix(group-table): Removed EditGroups
    Removed the edit grops components because the 'View Details' component was accomplishing the same goal.

--- crud/src/app/components/groups/GroupsTable.js
+++ crud/src/app/components/groups/GroupsTable.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-import EditGroup from './editGroup';
 import AddStudent from './addStudent';
 import ViewGroup from './viewDetails';
 
@@ -10,17 +9,11 @@ const TableRow = ({ item }) => {
   // Component is being rendered twice, for some reason...
   const [openMenu, setOpenMenu] = useState(false);
   const menuRef = useRef(null);
-  const [isEditGroupOpen, setIsEditGroupOpen] = useState(false);
   const [isViewGroupOpen, setIsViewGroupOpen] = useState(false);
   const [isAddStudentOpen, setIsAddStudentOpen] = useState(false);
 
   const toggleMenu = () => {
     setOpenMenu(!openMenu);
-  };
-
-  const openEditGroupModal = () => {
-    toggleMenu();
-    setIsEditGroupOpen(!isEditGroupOpen);
   };
 
   const openViewGroupModal = () => {
@@ -108,26 +101,6 @@ const TableRow = ({ item }) => {
                 </span>
               </div>
 
-              <div className="" onClick={openEditGroupModal} role="none">
-                <span className="flex items-center text-gray-500 font-medium hover:text-gray-900 hover:bg-gray-50 block px-4 py-2 text-sm">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="#000000"
-                    className="mr-2 h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
-                    />
-                  </svg>
-                  Edit
-                </span>
-              </div>
-
               <div className="" onClick={deleteGroup} role="none">
                 <span className="flex items-center text-gray-500 font-medium hover:text-gray-900 hover:bg-gray-50 block px-4 py-2 text-sm">
                   <svg
@@ -153,11 +126,6 @@ const TableRow = ({ item }) => {
         {isAddStudentOpen && (
           <AddStudent
             closeAddStudentModal={openAddStudentModal} 
-          />
-        )}
-        {isEditGroupOpen && (
-          <EditGroup 
-            closeEditGroupModal={openEditGroupModal} 
           />
         )}
 
