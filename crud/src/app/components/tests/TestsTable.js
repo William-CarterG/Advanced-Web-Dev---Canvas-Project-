d9c1b939261a07935898fbedcdf96b50dda2fd69
William-CarterG <wgcarter@miuandes.cl>
2023-07-04 09:23:17 -0400

    refactor(crud/*): questions table

--- crud/src/app/components/tests/TestsTable.js
+++ crud/src/app/components/tests/TestsTable.js
@@ -6,12 +6,9 @@ import Delete from '../../buttons/delete.js';
 import AddQuestion from '../../buttons/addQuestion.js';
 import AddTestQuestion from './question/crud/addQuestion.js';
 import startFetch from '../../../API';
+import QuestionsTable from './question/QuestionsTable.js';
 
-// Credits to TailwindComponents user 'BrendaMorales97' for 
-// creating a good part of this table.
-
-const TableRow = ({ item, setTests }) => {
-  // Component is being rendered twice, for some reason...
+const TableRow = ({ item, setTests, setSelectedTest }) => {
   const [openMenu, setOpenMenu] = useState(false);
   const menuRef = useRef(null);
   const [addTestVisible] = useState(false);
@@ -25,6 +22,7 @@ const TableRow = ({ item, setTests }) => {
   const testDetails = () => {
     toggleMenu();
     setViewTestVisible(!viewTestVisible);
+    setSelectedTest(item);
   };
 
   const addQuestion = () => {
@@ -39,7 +37,7 @@ const TableRow = ({ item, setTests }) => {
         setTests(data);
       });
     });
-  }
+  };
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -53,15 +51,14 @@ const TableRow = ({ item, setTests }) => {
     return () => {
       document.removeEventListener('click', handleClickOutside);
     };
-
   }, []);
+
   return (
     <tr key={item.name} className="border-b h-16 hover:bg-gray-50">
-      <td> {item.name} </td>
-      <td> {item.questions_count} </td>
+      <td>{item.name}</td>
+      <td>{item.questions_count}</td>
 
       <td>
-        {/*View Details, Add user, Edit, Delete menu */}
         <div ref={menuRef}>
           <div onClick={toggleMenu} className="flex items-center text-gray-400">
             <More />
@@ -81,24 +78,19 @@ const TableRow = ({ item, setTests }) => {
               </div>
             </div>
           )}
-          {viewTestVisible && (
-            <ViewTest closeViewTestModal={() => setViewTestVisible(!viewTestVisible)} setTests={setTests} item={item} />
-          )}
           {AddTestQuestionVisible && (
-            <AddTestQuestion toggleModelOpen={() => setAddTestQuestionVisible(!AddTestQuestionVisible)} setTests={setTests} item={item}/>
+            <AddTestQuestion toggleModelOpen={() => setAddTestQuestionVisible(!AddTestQuestionVisible)} setTests={setTests} item={item} />
           )}
-          {addTestVisible && (
-            <addTest />
-          )}
+          {addTestVisible && <addTest />}
         </div>
       </td>
     </tr>
   );
 };
 
-
 const TestTableComponent = ({ data, headers, setTests }) => {
-
+  const [seeTestTable, setSeeTestTable] = useState(true);
+  const [selectedTest, setSelectedTest] = useState(null);
   const [testData, setTestData] = useState(data);
 
   useEffect(() => {
@@ -107,29 +99,49 @@ const TestTableComponent = ({ data, headers, setTests }) => {
     });
   });
 
+  const goBack = () => {
+    setSelectedTest(null);
+  };
+
   return (
-    <div className="mt-10 flex flex-col h-[65vh] py-6">  
-      <div className="flex-grow rounded-2xl overflow-auto bg-white"> 
-        <table className="relative w-full border"> 
-          <thead className="sticky top-0 px-6 py-3 bg-gray-50">
-            <tr>
-              {headers.map((header, index) => (
-              <th key={index}
-                className="px-3 py-3 text-center text-lg font-semibold text-gray-900"
-              >
-                {header}
-              </th>
+    <div className="mt-10 flex flex-col h-[65vh] py-6">
+      {selectedTest && (
+        <button onClick={goBack} className="mb-4 ml-4 p-2 bg-white text-[#282c34] hover:bg-[#1d232e] hover:text-white rounded-md duration-500">Volver a pruebas</button>
+      )}
+      <div className="flex-grow rounded-2xl overflow-auto bg-white">
+        {selectedTest ? (
+          <div>
+            <QuestionsTable setTests={setTests} item={selectedTest} />
+          </div>
+        ) : (
+          <table className="relative w-full border">
+            <thead className="sticky top-0 px-6 py-3 bg-gray-50">
+              <tr>
+                {headers.map((header, index) => (
+                  <th
+                    key={index}
+                    className="px-3 py-3 text-center text-lg font-semibold text-gray-900"
+                  >
+                    {header}
+                  </th>
+                ))}
+                <th></th>
+              </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-200 bg-white">
+              {testData.map((item) => (
+                <TableRow
+                  key={item.id}
+                  item={item}
+                  setTests={setTests}
+                  setSelectedTest={setSelectedTest}
+                />
               ))}
-              <th></th>
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-gray-200 bg-white">
-            {testData.map((item) => (
-              <TableRow key={item.id} item={item} setTests={setTests}/>
-            ))}
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        )}
       </div>
+
     </div>
   );
 };
